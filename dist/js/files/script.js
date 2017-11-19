7a852e99357247c0562a0136ee5fc2b89739e780
DimitrijevasDominykas <dimitrijevas@gmail.com>
2017-11-19 16:46:35 +0200

    first commit

---
+++ dist/js/files/script.js
@@ -0,0 +1,22 @@
+var mainFoto= document.querySelector('section.landing-page-home');
+var fotoList= ['img/main1.jpeg','img/main.jpeg'];
+var arrow=document.querySelectorAll('.arrow');
+var arrowOne=document.querySelector('.fa');
+
+
+arrow.addEventListener('click',function(){
+    mainFoto.src=fotoList(activeFoto;)
+    arrow[activeFoto];
+    activeFoto++;
+    if(activeFoto==fotoList.length)activeFoto=0;
+    arrow[activeFoto]
+})
+
+
+
+
+
+
+console.log('App up and running..-');
+
+
